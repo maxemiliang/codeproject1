c1434e3c624f20c51aec6974243c3dd87e25378b
Restyled.io <commits@restyled.io>
2020-04-02 09:02:10 +0000

    Restyled by prettier

--- test.js
+++ test.js
@@ -1,10 +1,16 @@
 console.log("Hejsan från Robin ! ");
 
-function add(a, b) { return a + b; }
+function add(a, b) {
+  return a + b;
+}
 
-function hello1() { console.log("Hello world"); }
+function hello1() {
+  console.log("Hello world");
+}
 
 // Minus a - b
-function minus(a, b) { return a - b; }
+function minus(a, b) {
+  return a - b;
+}
 
 // https://www.youtube.com/watch?v=dQw4w9WgXcQ

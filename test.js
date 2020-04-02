88695f28544beb8fbebcc422fb932f040eeff495
Restyled.io <commits@restyled.io>
2020-04-02 09:02:09 +0000

    Restyled by clang-format

--- test.js
+++ test.js
@@ -1,16 +1,10 @@
 console.log("Hejsan från Robin ! ");
 
-function add(a, b) {
-    return a + b;
-}
+function add(a, b) { return a + b; }
 
-function hello1() {
-    console.log("Hello world");
-}
+function hello1() { console.log("Hello world"); }
 
 // Minus a - b
-function minus(a, b) {
-    return a - b;
-}
+function minus(a, b) { return a - b; }
 
-//https://www.youtube.com/watch?v=dQw4w9WgXcQ
+// https://www.youtube.com/watch?v=dQw4w9WgXcQ

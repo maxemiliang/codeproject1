aaa337cfd39aff437fc1f1af3e73a64c7480ea68
Restyled.io <commits@restyled.io>
2020-04-02 09:03:45 +0000

    Restyled by clang-format

--- test.js
+++ test.js
@@ -1,14 +1,8 @@
 console.log("Hejsan från Chrisu36 1!");
 
-function add(a, b) {
-    return a + b;
-}
+function add(a, b) { return a + b; }
 
-function hello1() {
-    console.log("Hello world");
-}
+function hello1() { console.log("Hello world"); }
 
 // Minus a - b
-function minus(a, b) {
-    return a - b;
-}
+function minus(a, b) { return a - b; }
